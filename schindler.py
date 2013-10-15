898ff590a336eca4760455e33c2f2ebe0c97046b
polyrabbit <mcx_221@foxmail.com>
2013-10-15 20:57:37 +0800

    Put Schindler'sList.txt where you start the program

--- schindler.py
+++ schindler.py
@@ -90,9 +90,10 @@ class AuthenticationHandler(BaseHTTPServer.BaseHTTPRequestHandler, object):
     server_version = "Schindler'sList/0.1"
     result_fname = "Schindler'sList.txt"
 
-    rfn = os.path.join(os.path.dirname(__file__), result_fname)
+    # Put Schindler'sList.txt where you start the program
+    # rfn = os.path.join(os.path.dirname(__file__), result_fname)
     # 0 means unbuffered, 1 means line buffered
-    fout = open(rfn, 'w', 1)
+    fout = open(result_fname, 'w', 1)
 
     def do_GET(self):
         ip = self.client_address[0]
